ba28fee3916cbf2c52c6ea62da192a250444f89a
Michal Stolarczyk <michal.stolarczyk@allegro.pl>
2019-11-12 19:39:16 +0100

    initial commit

---
+++ src/main/java/app/javapki/JavapkiApplication.java
@@ -0,0 +1,13 @@
+package app.javapki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavapkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavapkiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/app/javapki/JavapkiApplicationTests.java
@@ -0,0 +1,13 @@
+package app.javapki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavapkiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
