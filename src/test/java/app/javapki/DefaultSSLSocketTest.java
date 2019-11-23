08c275275c2d9e94a9e78f1aaef6b413366148b5
Michal Gryglicki <michal.gryglicki@allegro.pl>
2019-11-23 12:15:21 +0100

    Solution

--- src/test/java/app/javapki/DefaultSSLSocketTest.java
+++ src/test/java/app/javapki/DefaultSSLSocketTest.java
@@ -3,9 +3,14 @@ package app.javapki;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLHandshakeException;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManagerFactory;
+
+import java.io.InputStream;
+import java.security.KeyStore;
 
 import static app.javapki.HttpUtils.httpGet;
 import static app.javapki.HttpUtils.successOnLocalhostHostnameVerification;
@@ -44,7 +49,17 @@ public class DefaultSSLSocketTest extends BaseIntegrationTest {
         Assertions.assertDoesNotThrow(() -> {
             successOnLocalhostHostnameVerification();
 
-            SSLSocketFactory factory = (SSLSocketFactory) SSLSocketFactory.getDefault();
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
+            KeyStore ks = KeyStore.getInstance("JKS");
+
+            InputStream clientTrustStoreInputStream = DefaultSSLSocketTest.class.getResourceAsStream("/client_truststore.jks");
+            ks.load(clientTrustStoreInputStream, "qwerty".toCharArray());
+
+            tmf.init(ks);
+            ctx.init(null, tmf.getTrustManagers(), null);
+
+            SSLSocketFactory factory = ctx.getSocketFactory();
             SSLSocket socket = (SSLSocket) factory.createSocket("localhost", localServerPort);
 
             socket.startHandshake();
